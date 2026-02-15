f4af1d49b7ece7ee12fff24362838457ed036604
Frank Deppenmeier <frank@acf6aed1-bad7-4d9a-8dff-ad33328b3240.fritz.box>
2026-02-15 01:34:22 +0100

    chore: remove debug_users.php security risk

--- api/debug_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db.php';
-$pdo = getDBConnection();
-$stmt = $pdo->query("SELECT id, username, email FROM users");
-$users = $stmt->fetchAll();
-echo "<pre>" . print_r($users, true) . "</pre>";
-?>
